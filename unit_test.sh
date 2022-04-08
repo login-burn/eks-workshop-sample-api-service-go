d4075b50f1ce05499574f6b3e2cadd6e6ecf9900
chris93015 <chris93015@lsh.mod.gov.uk>
2022-04-08 16:48:29 +0100

    troubleshoot

--- unit_test.sh
+++ unit_test.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 set -e
+
+ingress_str=$(kubectl get svc hello-k8s -o json | jq -c '.status.loadBalancer.ingress')
+echo $ingress_str
 api_host=$(kubectl get svc hello-k8s -o json | jq -c '.status.loadBalancer.ingress' | jq -c '.[] | select(.hostname).hostname')
+echo $api_host
 curl -m 2 $api_host
