b7e55d827d5e419b3f77aa5bce6441d39e61ec19
chris93015 <chris93015@lsh.mod.gov.uk>
2022-04-08 16:38:24 +0100

    for sure this time

--- unit_test.sh
+++ unit_test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-api_host=$(kubectl get svc hello-k8s -o json | jq -c '.status.loadBalancer.ingress' | jq -c '.[] | select(.hostname).hostname'
+api_host=$(kubectl get svc hello-k8s -o json | jq -c '.status.loadBalancer.ingress' | jq -c '.[] | select(.hostname).hostname')
 curl -m 2 $api_host
