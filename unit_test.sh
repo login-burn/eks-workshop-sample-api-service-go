1a0e2a88fa2d764f04d20ccd70765d443af5cb7d
chris93015 <chris93015@lsh.mod.gov.uk>
2022-04-08 17:09:52 +0100

    quotes

--- unit_test.sh
+++ unit_test.sh
@@ -3,6 +3,6 @@ set -e
 
 ingress_str=$(kubectl get svc hello-k8s -o json | jq -c '.status.loadBalancer.ingress')
 echo $ingress_str
-api_host=$(kubectl get svc hello-k8s -o json | jq -c '.status.loadBalancer.ingress' | jq -c '.[] | select(.).ip')
-echo $api_host
+api_host=$(kubectl get svc hello-k8s -o json | jq -c '.status.loadBalancer.ingress' | jq -c '.[] | select(.).ip' | tr '"') 
+echo "http://$api_host"
 curl -m 2 $api_host
